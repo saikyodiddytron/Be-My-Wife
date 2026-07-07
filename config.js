349ebde3d7dd33628bcb12f114bd58d0fa5c79dc
saikyodiddytron <saikyotsbg@gmail.com>
2026-07-06 21:22:39 -0400

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ const CONFIG = {
             text: "Do you love me?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "! ❤️"           // Secret hover message
+                
         },
         second: {
             text: "How much do you love me?",                          // For the love meter
